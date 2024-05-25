1f72481475ed9cbb59e8b758d2c9b59c2d8b817e
MarcuTamas <tamasmarcu@outlook.com>
2024-05-25 20:24:19 +0300

    Enrollment FE form almost done

--- SalonWebApp/src/app/components/enrollment/enrollment.component.ts
+++ SalonWebApp/src/app/components/enrollment/enrollment.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-enrollment',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './enrollment.component.html',
-  styleUrl: './enrollment.component.css'
+  styleUrl: './enrollment.component.css',
 })
-export class EnrollmentComponent {
+export class EnrollmentComponent implements OnInit{
+  enrollmentForm: FormGroup;
 
+  ngOnInit() {
+    this.enrollmentForm = new FormGroup({
+      'firstName': new FormControl(null, Validators.required),
+      'lastName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.pattern(/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/)]),
+      'confirmPassword':  new FormControl(null, [Validators.required, Validators.pattern(/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/)]),
+      'agreeTerms': new FormControl(false)
+    });
+  }
+
+
+  onSubmit() {
+    console.log(this.enrollmentForm)
+  }
 }
