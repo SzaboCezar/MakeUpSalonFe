f1455b5ef3b1876338a60423eba4294ea66ef274
MarcuTamas <tamasmarcu@outlook.com>
2024-06-10 14:31:29 +0300

    Done password reset

--- SalonWebApp/src/app/auth/password/reset-password/reset-password.component.ts
+++ SalonWebApp/src/app/auth/password/reset-password/reset-password.component.ts
@@ -3,9 +3,7 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 import {PasswordService} from "../password.service";
 import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {NgIf} from "@angular/common";
-import {ResetPasswordEmailService} from "./reset-password-email/reset-password-email.service";
 import {ResetPasswordRequest} from "../../../shared/models/ResetPasswordRequest.model";
-
 
 @Component({
   selector: 'app-reset-password',
@@ -22,40 +20,40 @@ export class ResetPasswordComponent implements OnInit {
   error: string = null;
 
   constructor(
-    private resetPasswordService: PasswordService,
-    private resetPasswordEmailService: ResetPasswordEmailService,
+    private passwordService: PasswordService,
     private router: Router,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute
+  ) {}
 
-  //TODO: enable validators.
   ngOnInit(): void {
     this.getParams();
 
-    if(this.resetPasswordEmailService.checkEmailToken(this.email, this.emailToken)) {
-      this.areParamsValid = true;
-      this.resetForm = new FormGroup({
-        // newPassword: new FormControl(null, [Validators.required, Validators.pattern(/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/)]),
-        newPassword: new FormControl(null, Validators.required),
-        // confirmPassword: new FormControl(null, [Validators.required, Validators.pattern(/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/)])
-        confirmationPassword: new FormControl(null, Validators.required)
-      });
-    }
+    this.resetForm = new FormGroup({
+      newPassword: new FormControl(null, [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/)]),
+      confirmationPassword: new FormControl(null, Validators.required)
+    });
   }
 
   onSubmit() {
-    console.log(this.resetForm);
+    if (this.resetForm.get('newPassword').value !== this.resetForm.get('confirmationPassword').value) {
+      this.error = 'Passwords do not match!';
+      return;
+    }
+
     const resetPasswordRequest: ResetPasswordRequest = this.resetForm.getRawValue();
-    console.log("ResetPasswordComponent |Reset password request: ", resetPasswordRequest)
 
-
-    this.resetPasswordService.resetPassword(resetPasswordRequest, this.email).subscribe(
-      data => {
-        console.log(data);
+    this.passwordService.resetPassword(resetPasswordRequest, this.email).subscribe(
+      () => {
         this.router.navigate(['/auth']);
       },
-      error => {
-        console.log(error);
-        this.error = "Password was not reset!";
+      (error) => {
+        if (error.status === 400) {
+          this.error = 'Invalid request. Please check your input.';
+        } else if (error.status === 404) {
+          this.error = 'Resource not found. Please try again later.';
+        } else {
+          this.error = 'An unexpected error occurred. Please try again later.';
+        }
       }
     );
   }
@@ -63,18 +61,20 @@ export class ResetPasswordComponent implements OnInit {
   getParams() {
     this.route.paramMap.subscribe(params => {
       const emailAndToken = params.get('emailAndToken');
-      if (emailAndToken) {
+      if (emailAndToken && this.passwordService.checkEmailTokenInLocalStorage()) {
         const [email, token] = this.extractEmailAndToken(emailAndToken);
         this.email = email;
         this.emailToken = token;
-        console.log('Email:', this.email);
-        console.log('Token:', this.emailToken);
+        this.areParamsValid = true;
+      } else {
+        this.error = 'No email and token provided. Please try again.'
       }
     });
   }
 
-  extractEmailAndToken(emailAndToken: string): [string, string] {
+  private extractEmailAndToken(emailAndToken: string): [string, string] {
     const [email, token] = emailAndToken.split(':');
     return [decodeURIComponent(email), token];
   }
+
 }
