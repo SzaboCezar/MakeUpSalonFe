32ca352e1909fc11277507d372ed82a408fb7072
MarcuTamas <tamasmarcu@outlook.com>
2024-06-11 16:23:09 +0300

    Improved the UI

--- SalonWebApp/src/app/components/treatment/treatment-list/treatment-list.component.ts
+++ SalonWebApp/src/app/components/treatment/treatment-list/treatment-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {Component, ElementRef, NgZone, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { Treatment } from '../../../shared/models/Treatment.model';
 import { TreatmentService } from '../../../services/treatment.service';
 import { CommonModule, DatePipe } from '@angular/common';
@@ -8,16 +8,23 @@ import {
   NgbAccordionCollapse,
   NgbAccordionDirective,
   NgbAccordionHeader,
-  NgbAccordionItem,
+  NgbAccordionItem, NgbModal,
   NgbTooltip,
 } from '@ng-bootstrap/ng-bootstrap';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { Subscription, forkJoin } from 'rxjs';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {Subscription, forkJoin, catchError, of} from 'rxjs';
 import { map } from 'rxjs/operators';
 import { NavBarComponent } from '../../dom-element/nav-bar/nav-bar.component';
 import { LoadingSpinnerComponent } from '../../dom-element/loading-spinner/loading-spinner.component';
 import { PersonService } from '../../../services/person.service';
 import { Person } from '../../../shared/models/Person.model';
+import { Location } from '@angular/common';
+
+declare global {
+  interface Window {
+    bootstrap: any;
+  }
+}
 
 @Component({
   selector: 'app-treatment-list',
@@ -44,11 +51,20 @@ export class TreatmentListComponent implements OnInit, OnDestroy {
   selectedTreatment?: Treatment;
   treatments: Treatment[];
   selectedEmployeeTreatments: { [key: number]: Person[] } = {};
+  error: string = null;
+  private modalInstance: any;
+
+  @ViewChild('errorModal') errorModal: ElementRef;
+
 
   constructor(
     private treatmentService: TreatmentService,
     private personService: PersonService,
-    private route: ActivatedRoute
+    private location: Location,
+    private route: ActivatedRoute,
+    private router: Router,
+    private modalService: NgbModal,
+    private ngZone: NgZone
   ) {}
 
   ngOnInit(): void {
@@ -56,10 +72,14 @@ export class TreatmentListComponent implements OnInit, OnDestroy {
       this.treatmentService.treatmentsChanged.subscribe(
         (treatments: Treatment[]) => {
           this.treatments = treatments;
+          this.selectedTreatment = treatments.length > 0 ? treatments[0] : undefined; // Setare primul tratament din listă ca fiind selectat sau undefined dacă nu există niciun tratament
         }
       );
 
     this.treatments = this.treatmentService.getTreatments();
+    if (this.treatments.length > 0) {
+      this.selectedTreatment = this.treatments[0]; // Setare primul tratament din listă ca fiind selectat sau undefined dacă nu există niciun tratament
+    }
     console.log('treatments: ', this.treatments);
     this.initializeEmployeeTreatments();
   }
@@ -97,6 +117,43 @@ export class TreatmentListComponent implements OnInit, OnDestroy {
   onSelect(treatment: Treatment): void {
     this.selectedTreatment = treatment;
     this.treatmentService.getTreatment(treatment.treatmentID);
+  }
+
+  onConfirm(): void {
+    if (this.selectedTreatment) {
+      this.treatmentService.deleteTreatment(this.selectedTreatment.treatmentID)
+        .pipe(
+          catchError((error) => {
+            this.error = error.message;
+            this.openModal(); // Deschide modalul de eroare
+            return of(null); // Returnarea unui observable gol pentru a preveni răspândirea erorii către subscriberi
+          })
+        )
+        .subscribe(() => {
+          this.error = null; // Resetarea erorii la null
+          this.router.navigate(['/treatments']); // Redirecționarea către pagina de tratamente
+        });
+    }
+  }
+
+  openModal() {
+    const modalElement = document.getElementById('errorModal');
+    if (modalElement) {
+      this.modalInstance = new window.bootstrap.Modal(modalElement);
+      this.modalInstance.show();
+    }
+  }
+
+  onCancel(): void {
+    if (this.modalInstance) {
+      this.modalInstance.hide();
+      this.error = null;
+    }
+  }
+
+  handleImageError(event): void {
+    // Dacă imaginea specificată în treatment.pictureUrl nu poate fi încărcată, afișăm imaginea de rezervă
+    event.target.src = 'assets/img/HomePage/RealisticMakeup.png';
   }
 
   ngOnDestroy() {
